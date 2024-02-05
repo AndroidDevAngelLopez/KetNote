2e9889138dc4e0e7e09fe018932a8d78846ad13c
Angel Lopez <angelcecyt09@gmail.com>
2024-02-05 17:17:03 -0600

    KetNote 2402 RC-2 added Gemini support.

--- app/src/main/java/com/complexsoft/ketnote/utils/Constants.kt
+++ app/src/main/java/com/complexsoft/ketnote/utils/Constants.kt
@@ -1,7 +1,7 @@
 package com.complexsoft.ketnote.utils
 
 object Constants {
-    const val APP_VERSION = "2401-RC-2 Trinity"
+    const val APP_VERSION = "2402-RC-2 Trinity"
     const val IMAGES_DATABASE = "images_db"
     const val IMAGE_TO_UPLOAD_TABLE = "image_to_upload_table"
     const val IMAGE_TO_DELETE_TABLE = "image_to_delete_table"
